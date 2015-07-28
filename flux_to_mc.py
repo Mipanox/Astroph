497486250389d28597d02dd4bdc251da574a3d54
Mipanox <vcubewca@gmail.com>
2015-07-28 20:29:06 +0800

    update single-dish: sd=True

--- flux_to_mc.py
+++ flux_to_mc.py
@@ -6,13 +6,15 @@ from astropy.utils.data import get_readable_fileobj
 from astropy.io import fits
 
 class ds_flux(object):
-    def __init__(self,line,T_ex,area,\
+    def __init__(self,line,T_ex,area,sd=False,eta=None, \
                  dis=None,beam=None,cell=None,linewidth=None, \
                  vrange=None,dataset=None,flux=None):
         self.Tx  = T_ex
         self.dis = dis
         self.vr  = vrange
         self.ll  = line
+        self.sd  = sd   # check if input is single-dish
+        self.eta = eta
         self.pg  = area # polygon path if ds=True ;
                         # area (arcsec^2) if flux_T given
         
@@ -28,14 +30,14 @@ class ds_flux(object):
         if dataset:
             with get_readable_fileobj(dataset, cache=True) as f:
                 self.fitsfile = fits.open(f)
-                self.data     = self.fitsfile[0].data
                 self.header   = self.fitsfile[0].header
+                self.data     = self.fitsfile[0].data
             
             self.bmaj = self.header["bmaj"]*3600.
             self.bmin = self.header["bmin"]*3600.
-            self.cx   = self.header["cdelt1"]*3600.
-            self.cy   = self.header["cdelt1"]*3600.
-            self.lw   = self.header["cdelt3"]/1000. # in km/s
+            self.cx   = np.abs(self.header["cdelt1"]*3600.)
+            self.cy   = np.abs(self.header["cdelt1"]*3600.)
+            self.lw   = np.abs(self.header["cdelt3"]/1000.) # in km/s
         else:
             self.bmaj = beam[0]
             self.bmin = beam[1]
@@ -43,62 +45,95 @@ class ds_flux(object):
             self.cy   = cell[1]
             self.lw   = linewidth
 
-        if flux_T: self.fx = np.array(flux)
-        else:      self.fx = self.flux()
+        if flux: self.fx = np.array(flux)
+        else:    self.fx = self.flux()
 
-    def polyarea(self):
-    ###--return area of the polygon region in arcsec^2--###
-        if type(self.pg) == int: return self.pg
+    def _poaa(self):
+    ###--return polygon area in pixel number--###
+        #how many arcsec squared in one pixel
+        ap = self.cx * self.cy
+        if type(self.pg) == int: return self.pg / ap
         else:
         #mask of the polygon
-            mk = np.zeros((self.data.shape[-1],self.data.shape[-2]))
-            km = np.ones ((self.data.shape[-1],self.data.shape[-2]))
+            if self.header["naxis"] == 3: da = self.data
+            else                        : da = self.data[0]
+            mk = np.zeros((da.shape[-1],da.shape[-2]))
+            km = np.ones ((da.shape[-1],da.shape[-2]))
             cv2.fillConvexPoly(mk, np.array(self.pg), 1)
 
             mk  = mk.astype(np.bool)
-            out = np.zeros_like(self.data[0,0])
-
-            #how many " per pixel
-            cv = np.abs(self.header["cdelt1"]*3600)
-            return np.sum(km[mk])*cv**2
+            out = np.zeros_like(da[0])
             
-    def flux(self):
-    ###--return flux in Jy/beam in the specified polygon--###
+            return np.sum(km[mk])
+
+    def polyarea(self):
+    ###--return area of the polygon region in arcsec^2--###
+        ap = self.cx * self.cy
+        return self._poaa() * ap
+
+    def _sm(self):
+    ###--sum of pixel values--###
         #mask of the polygon
-        mk = np.zeros((self.data.shape[-1],self.data.shape[-2]))
+        if self.header["naxis"] == 3: da = self.data
+        else                        : da = self.data[0]
+        mk = np.zeros((da.shape[-1],da.shape[-2]))
         cv2.fillConvexPoly(mk, np.array(self.pg), 1)
 
         mk  = mk.astype(np.bool)
-        out = np.zeros_like(self.data[0,0])
+        out = np.zeros_like(da[0])
         
         fx = []
         if self.vr:
-            vii = int((self.vr[0] - self.header["crval3"]/1000.) \
+            vii = int(self.header["crpix3"]+ \
+                     (self.vr[0]-self.header["crval3"]/1000.) \
                       /(self.header["cdelt3"]/1000.))-1
-            vff = int((self.vr[1] - self.header["crval3"]/1000.) \
+            vff = int(self.header["crpix3"]+ \
+                     (self.vr[1]-self.header["crval3"]/1000.) \
                       /(self.header["cdelt3"]/1000.))-1
             rg = range(vii,vff)
-        else: rg = range(self.data.shape[-3])
+        else: rg = range(da.shape[-3])
+
+        #sum
         for zz in rg:
-            out[mk] = self.data[0,zz][mk]
+            out[mk] = da[zz][mk]
             fx.append(np.sum(out))
         return np.array(fx)
-
+            
+    def flux(self):
+    ###--return flux in Jy/beam in the specified polygon--###
+        #radians squared to degrees squared
+        cv = ((u.rad).to(u.deg))**2
+        bm = 2*np.pi*self.bmaj*self.bmin/cv
+        
+        if self.sd: return self.flux_density()*bm
+        else:       return self._sm()
+            
     def flux_density(self):
     ###--return flux density (Jy)---###
+        #radians squared to arcsec squared
+        ca = ((u.rad).to(u.arcsec))**2
+
         #beam area = beam size / cell size
         bm = 2*np.pi*self.bmaj*self.bmin/ \
              (2*np.sqrt(2*np.log(2)))**2/(self.cx*self.cy)
-        return self.fx / bm
+             
+        if self.sd:
+            cv = 2*k_B.value*self.ln[self.ll][2]**2/c.value**2*10**26
+            return self.rj_tb() * cv * self.polyarea() / ca
+ 
+        else: return self.fx / bm
 
     def rj_tb(self):
     ###--return temperature (Rayleigh-Jeans limit)--###
         #radians squared to arcsec squared
         ca = ((u.rad).to(u.arcsec))**2
 
-        return self.flux_density()*c.value**2*10**-26/ \
-               2/k_B.value/self.ln[self.ll][2]**2/ \
-               self.polyarea()*ca
+        if self.sd:
+            return self._sm() / self.eta / self._poaa()
+        else:
+            return self.flux_density()*c.value**2*10**-26/ \
+                   2/k_B.value/self.ln[self.ll][2]**2/ \
+                   self.polyarea()*ca
                
     ###--a simple function for convenience---###
     def f(self,x): return (np.exp(x)-1)**-1
@@ -115,10 +150,11 @@ class ds_flux(object):
 
     def col_d_tot(self):
     ###--return total column density of the line species (cm^-2)--###
+        lwd  = np.abs(self.lw)
         #channel width in Hz
         ftov = u.doppler_radio(self.ln[self.ll][2]*u.Hz)
         lw   = self.ln[self.ll][2]- \
-               (self.lw*(u.km/u.s)).to(u.Hz,equivalencies=ftov).value
+               (lwd*(u.km/u.s)).to(u.Hz,equivalencies=ftov).value
         
         #upper column density
         to = h.value/k_B.value*self.ln[self.ll][2]
@@ -126,7 +162,7 @@ class ds_flux(object):
         
         n_u = 8*np.pi*self.ln[self.ll][2]**2/(c.value*100.)**2* \
               self.f(ex)/self.ln[self.ll][0]* \
-              np.sum(self.rj_tb())*self.lw
+              np.sum(self.rj_tb())*lwd
 
         #partition function, approximated by integral
         eu = k_B.value*self.Tx/h.value/self.ln[self.ll][1]
