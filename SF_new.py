bc671afdfccd149c0b64512b99d9c1b4e686d447
Mipanox <vcubewca@gmail.com>
2016-05-11 18:17:07 +0800

    updated histogram

--- SF_new.py
+++ SF_new.py
@@ -1,9 +1,7 @@
 '''
 see the ipython notebook in ~/Desktop/coding_temp for usage and development
-Updated: 2016/05/10:
- - handling of nan values
- - added pol and difference (default 180)
- - drawing
+Updated: 2016/05/11:
+ - normalized histogram
 '''
 
 import os
@@ -151,73 +149,87 @@ class SF(object):
             ln = np.unique(np.sort(np.array([(m*m+n*n) for m in range(s) for n in range(m,b)])))
             ct = np.zeros(len(ln)) # count if sf[ix] has been filled with how many pairs
             sf = np.zeros(len(ln))
+            hs = [[] for _ in range(len(ln))] # for histogram, throw in raw data
         
             xv = ar.shape[0]
             yv = ar.shape[1]
             
             for (x,y),i in np.ndenumerate(ar): # run through all index pairs
                 ll = x**2+y**2
-                ix = np.where(ln==ll)
+                ix = np.where(ln==ll)[0][0]
         
                 if x==0 and y!=0:
                     aa = np.pad(ar,((0,0),(y,0)),
                                 mode='constant', constant_values=(np.nan))[:, :-y]
                     nb = np.sum(overlap(ar,aa))
-                       
-                    if nb+ct[ix]==0: continue # avoid dividing zero
-                    else:
-                        sf[ix] = (sf[ix]*ct[ix] + np.nansum(cp(ar,aa)**od))/(nb + ct[ix]) # new average
-                    ct[ix] += nb
             
+                    r1 = cp(ar,aa)
+                    if nb+ct[ix]==0: continue
+                    else:
+                        sf[ix] = (sf[ix]*ct[ix] + np.nansum(r1**od))/(nb + ct[ix]) # new average
+                    ct[ix] += nb
+                    hs[ix] += r1[~np.isnan(r1)].flatten().tolist() # get all pairs and append to corresponding list
+
                 elif y==0 and x!=0:
                     aa = np.pad(ar,((x,0),(0,0)),
                                 mode='constant', constant_values=(np.nan))[:-x, :]
                     nb = np.sum(overlap(ar,aa))
-
+            
+                    r1 = cp(ar,aa)
                     if nb+ct[ix]==0: continue
                     else:
-                        sf[ix] = (sf[ix]*ct[ix] + np.nansum(cp(ar,aa)**od))/(nb + ct[ix]) 
+                        sf[ix] = (sf[ix]*ct[ix] + np.nansum(r1**od))/(nb + ct[ix]) 
                     ct[ix] += nb
-            
+                    hs[ix] += r1[~np.isnan(r1)].flatten().tolist()
+
                 elif x!=0 and y!=0:
                     aa = np.pad(ar,((x,0),(y,0)),
                                 mode='constant', constant_values=(np.nan))[:-x, :-y]
                     bb = np.pad(ar,((0,x),(y,0)),
                                 mode='constant', constant_values=(np.nan))[x:, :-y] # negative configuration
-                    nb = 2.*np.sum(overlap(ar,aa)) # number of computed pairs, 2 for aa & bb
+                    nb = 2.*np.sum(overlap(ar,aa))
+                    # number of computed pairs
 
+                    r1,r2 = cp(ar,aa),cp(ar,bb)
                     if nb+ct[ix]==0: continue
                     else:
-                        sf[ix] = (sf[ix]*ct[ix] + np.nansum(cp(ar,aa)**od) \
-                                                + np.nansum(cp(ar,bb)**od))/(nb + ct[ix])
+                        sf[ix] = (sf[ix]*ct[ix] + np.nansum(r1**od) \
+                                                + np.nansum(r2**od))/(nb + ct[ix])
                     ct[ix] += nb
-                
+                    hs[ix] += r1[~np.isnan(r1)].flatten().tolist() + \
+                              r2[~np.isnan(r2)].flatten().tolist()
+    
             ## bin
             ls = np.sqrt(ln)
             bs = np.arange(ls[0], round(ls[-1])+1, bn) - 1.e-12 # make sure no ambiguity for integers
             (counts, _) = np.histogram(ls, bs)
             idx = counts.cumsum() # binning indices for sf
-        
-            sf_,ln_ = [],[]
+    
+            sf_,ln_,hs_ = [],[],[]
             j = 1
             for i in idx:
                 if i==1: continue
-                elif np.sum(ct[j:i])==0: 
-                    sf_.append(0.)
-                    ln_.append(0.) # avoid divided by zero
                 else:
-                    sf_.append(np.sum((sf[j:i]*ct[j:i])/np.sum(ct[j:i]))**(1./od)) # weighted average of sf
-                    ln_.append(np.sum(ls[j:i]*ct[j:i])/np.sum(ct[j:i])) # weighted average of ls
+                    if np.sum(ct[j:i])==0: 
+                        sf_.append(0.)
+                        ln_.append(0.)  # avoid divided by zero
+                        hs_.append([0.])
+                    else:
+                        sf_.append(np.sum((sf[j:i]*ct[j:i])/np.sum(ct[j:i]))**(1./od)) # weighted average of sf
+                        ln_.append(np.sum(ls[j:i]*ct[j:i])/np.sum(ct[j:i])) # weighted average of ls
+                        hs_.append(reduce(lambda x, y: x + y, hs[j:i]))
                 j = i
             if np.sum(ct[j:])==0:
                 sf_.append(0.)
                 ln_.append(0.)
+                hs_.append([0.])
             else:
                 sf_.append(np.sum((sf[j:]*ct[j:])/np.sum(ct[j:]))**(1./od)) # don't forget the last bin
                 ln_.append(np.sum(ls[j:]*ct[j:])/np.sum(ct[j:]))
+                hs_.append(reduce(lambda x, y: x + y, hs[j:]))
         
-            return sf_,ln_
-            
+            return sf_,ln_,hs_
+        
         a = self.grd
         b = self.po + 90.     # convert to B-field
         
@@ -228,19 +240,33 @@ class SF(object):
             return _sf(cp(gf,b)),'difference (directionless)'
     
     def wrfile(self):
-        ((sff,dis),wh) = self._sf()
+        ((sff,dis,his),wh) = self._sf()
         
         pp = PdfPages('SF_%s.pdf' %self.nm)
         plt.figure()
-        plt.clf()
         
-        plt.scatter(dis,sff,s=10,c='b',edgecolors='none')
+        plt.clf()
+        plt.scatter(dis,sff,s=12,c='b',edgecolors='none')
         plt.xlabel('distance (pixel)')
         plt.ylabel('angle difference')
         plt.xlim(0,max(dis)+1)
         plt.ylim(0,90.)
-        
         plt.title('%s: Structure Function for %s (Order %s)' %(self.nm,wh,int(self.od)))
         pp.savefig()
+        
+        bn = 5.
+        for i in range(len(dis)):
+            if dis[i] > 0:
+                hist,bins = np.histogram(np.array(his[i]),range=(0.,90.),
+                                         bins=90/bn,density=True) # 5 deg per bin, prob. density func.
+                center = (bins[:-1] + bins[1:])/2
+            
+                plt.clf()
+                plt.bar(center, hist, align='center',width=bn)
+                plt.xlim(0,90)
+                plt.xlabel('Angle difference (deg)')
+                plt.ylabel('Normalized number of pairs in each bin (%s deg)' %bn)
+                plt.title("Histogram at scale %s (pixel)" %round(dis[i],2))
+                pp.savefig()
             
         pp.close()
