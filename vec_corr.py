bcd21f73a8af326deb3f509406bcd63f38d233cc
Mipanox <vcubewca@gmail.com>
2016-07-17 11:05:21 +0800

    temp update for translation

--- vec_corr.py
+++ vec_corr.py
@@ -18,12 +18,12 @@ class Vec_Corr(object):
     def _rdn_v(self):
         return np.random.random(self.v1.shape)
     
-    def corr_c(self):
+    def corr_c(self,n=None):
         ## Crosby 1993; rho^2 in [0,2]
         v1_x,v1_y = self.v1[0].flatten(),self.v1[1].flatten()
         v2_x,v2_y = self.v2[0].flatten(),self.v2[1].flatten()
         
-        n = v1_x.size
+        if n==None: n = v1_x.size  
         
         v_ = np.vstack([v1_x,v1_y,v2_x,v2_y])
         vv = v_ - v_.mean(axis=1).reshape(4,1) # subtraction of mean
@@ -34,12 +34,12 @@ class Vec_Corr(object):
         
         return np.trace( (S[0:2,2:4]*S[2:4,0:2]) / (S[0:2,0:2]*S[2:4,2:4]) )
     
-    def corr_h(self):
+    def corr_h(self,n=None):
         ## Hanson 1992; rho^2 in [0,1]
         v1_x,v1_y = self.v1[0].flatten(),self.v1[1].flatten()
         v2_x,v2_y = self.v2[0].flatten(),self.v2[1].flatten()
         
-        n = v1_x.size
+        if n==None: n = v1_x.size
         
         v1_c = v1_x + v1_y * 1j
         v2_c = v2_x + v2_y * 1j
@@ -76,3 +76,43 @@ class Vec_Corr(object):
         plt.legend(loc='upper right',fontsize=24)
         
         plt.show()
+        
+    def crd_tran(self):
+        ## coordinate translation
+        v1 = self.v1
+        v2 = self.v2
+        
+        rho_c,rho_h,(xx,yy)=0.,0.,(0,0)
+        for (x,y),i in np.ndenumerate(v1_x):
+            v1_ = np.pad(v1,((0,0),(x,0),(y,0)), mode='constant', 
+                         constant_values=np.nan)[:,:-x, :-y]
+            v2_ = np.pad(v2,((0,0),(x,0),(y,0)), mode='constant', 
+                         constant_values=np.nan)[:,:-x, :-y]
+            
+            n = np.nansum(v1_x/v1_x) / 2 # overlapping number of points
+            
+            v1_[np.isnan(v1_x)] = 0 # convert to zero for matrix arithmetics
+            v2_[np.isnan(v2_x)] = 0
+            
+            # v1 translated
+            self.v1 = v1_
+            self.v2 = v2
+            
+            cor_c = self.corr_c(n=n)
+            cor_h = self.corr_h(n=n)
+            
+            if cor_c > rho_c: rho_c = cor_c; (xx,yy)=(-x,-y) # negative for v1 tran.
+            if cor_h > rho_h: rho_h = cor_h; (xx,yy)=(-x,-y)
+        
+            # v2 translated
+            self.v1 = v1
+            self.v2 = v2_
+        
+            cor_c = self.corr_c(n=n)
+            cor_h = self.corr_h(n=n)
+            
+            if cor_c > rho_c: rho_c = cor_c; (xx,yy)=(x,y) # positive for v2 tran.
+            if cor_h > rho_h: rho_h = cor_h; (xx,yy)=(x,y)
+            
+        return rho_c,rho_h,(xx,yy)
+    
